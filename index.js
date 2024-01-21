109230a7b85ea8b3c0fa0e3c6ff14f45c6107fdc
Siphosihle Nomfanelo Mtshali <106113368+creator-droid@users.noreply.github.com>
2024-01-21 13:04:07 +0200

    added slideshow

--- index.js
+++ index.js
@@ -40,3 +40,35 @@ scroll.reveal(".work-img", {});
 // contact scroll
 scroll.reveal(".contact-container", {});
 scroll.reveal(".social-container", { delay: 200 });
+
+let slideIndex = 0;
+
+function showSlides() {
+  let i;
+  const slides = document.getElementsByClassName("mySlides");
+  const dots = document.getElementsByClassName("dot");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+
+  setTimeout(showSlides, 3000); // Change slide every 3 seconds
+}
+
+// Run the slideshow when the page loads
+window.onload = function () {
+  showSlides();
+};
